cdb4b16a43766521a03d3defd70d347e7a53e283
Shannongreen01 <shannon.green@bryanuniversity.edu>
2022-06-17 13:28:48 -0400

    added to Do

---
+++ my-app/src/toDo.js
@@ -0,0 +1,17 @@
+
+function ToDoList () {
+    return(
+
+        <div>
+            <h1>List of Todos</h1>
+            <ul>
+                <input type="checkbox"/>goto gym<br></br>
+                <input type="checkbox"/>do laundry<br></br>
+                <input type="checkbox"/>food shopping<br></br>
+                <input type="checkbox"/>do homework<br></br>
+            </ul>
+        </div>
+    );
+}
+
+export default ToDoList;
