94e32bc88b1aea395fd4685d5a7b9aecae34fba9
Shannongreen01 <96604457+Shannongreen01@users.noreply.github.com>
2022-06-24 09:47:36 -0400

    Update toDo.js

--- my-app/src/toDo.js
+++ my-app/src/toDo.js
@@ -1,16 +1,14 @@
 
-function ToDoList () {
-    return(
 
-        <div>
-            <h1>List of Todos</h1>
-            <ul>
-                <input type="checkbox"/>goto gym<br></br>
-                <input type="checkbox"/>do laundry<br></br>
-                <input type="checkbox"/>food shopping<br></br>
-                <input type="checkbox"/>do homework<br></br>
-            </ul>
-        </div>
+
+function ToDoList({todoItems}) {
+
+    const todoList = todoItems.map((todo, index) => <li key = {index}>{todo.text}</li>)
+
+    return (   
+        <h1>
+            <ul>{todoList}</ul>
+        </h1>
     );
 }
 
